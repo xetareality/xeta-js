b9e23d9fa1cd794b239a6c59d7d028e9c20d0183
xetadev <dev@xetareality.com>
2022-02-14 09:09:50 -0300

    feat: refactor fetch params

--- src/modules/resource.ts
+++ src/modules/resource.ts
@@ -9,17 +9,16 @@ export const Resource = {
     read: async ({type, key, sort=null, sortValue=null, fields=null, preview=null, fetch=null}) => {
         if (!['allowance', 'balance', 'candle', 'claim', 'object', 'pool', 'statistic', 'token', 'transaction', 'transfer', 'wallet'].includes(type)) throw Error('type:invalid')
 
-        return (fetch || $fetch)(Config.interface+'/read', {
-            method: 'GET',
-            params: Utils.strip({
-                type: type,
-                keyValue: key,
-                sort: sort,
-                sortValue: sortValue,
-                preview: preview,
-                dev: Config.dev,
-            }),
-        }).catch(e => {
+        var params = new URLSearchParams(Utils.strip({
+            type: type,
+            keyValue: key,
+            sort: sort,
+            sortValue: sortValue,
+            preview: preview,
+            dev: Config.dev,
+        }) as any).toString()
+
+        return (fetch || $fetch)(Config.interface+'/read?'+params).catch(e => {
             throw Error(e.data)
         })
     },
@@ -29,17 +28,16 @@ export const Resource = {
     list: async ({type, keys, sort=null, sortValues=null, fields=null, preview=null, fetch=null}) => {
         if (!['allowance', 'balance', 'candle', 'claim', 'object', 'pool', 'statistic', 'token', 'transaction', 'transfer', 'wallet'].includes(type)) throw Error('type:invalid')
 
-        return (fetch || $fetch)(Config.interface+'/list', {
-            method: 'GET',
-            params: Utils.strip({
-                type: type,
-                keyValues: keys.join(','),
-                sort: sort,
-                sortValues: sortValues,
-                preview: preview,
-                dev: Config.dev,
-            }),
-        }).catch(e => {
+        var params = new URLSearchParams(Utils.strip({
+            type: type,
+            keyValues: keys.join(','),
+            sort: sort,
+            sortValues: sortValues,
+            preview: preview,
+            dev: Config.dev,
+        }) as any).toString()
+
+        return (fetch || $fetch)(Config.interface+'/list?'+params).catch(e => {
             throw Error(e.data)
         })
     },
@@ -53,23 +51,22 @@ export const Resource = {
         if (['candle', 'statistic'].includes(type)) limit = limit ? Math.min(limit, 1000) : 200
         else limit = limit ? Math.min(limit, 25) : 25
 
-        return (fetch || $fetch)(Config.interface+'/scan', {
-            method: 'GET',
-            params: Utils.strip({
-                type: type,
-                keyValue: keyValue,
-                index: index,
-                indexValue: indexValue,
-                sort: sort,
-                sortValue: sortValue,
-                operator: operator,
-                asc: asc,
-                limit: limit,
-                preview: preview,
-                extend: extend,
-                dev: Config.dev,
-            }),
-        }).catch(e => {
+        var params = new URLSearchParams(Utils.strip({
+            type: type,
+            keyValue: keyValue,
+            index: index,
+            indexValue: indexValue,
+            sort: sort,
+            sortValue: sortValue,
+            operator: operator,
+            asc: asc,
+            limit: limit,
+            preview: preview,
+            extend: extend,
+            dev: Config.dev,
+        }) as any).toString()
+
+        return (fetch || $fetch)(Config.interface+'/scan?'+params).catch(e => {
             throw Error(e.data)
         })
     },
