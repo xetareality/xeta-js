0cd612af6b0bd79068886719fb8dca7b1319bd7f
xetadev <dev@xetareality.com>
2022-02-14 15:10:44 -0300

    fix: address returned data

--- src/modules/address.ts
+++ src/modules/address.ts
@@ -8,11 +8,8 @@ export const Address = {
     read: async ({address, fetch=null}) => {
         var params = new URLSearchParams({address: address, dev: Config.dev} as any).toString()
 
-        var result = (fetch || $fetch)(Config.interface+'/address?'+params).catch(e => {
+        return (fetch || $fetch)(Config.interface+'/address?'+params).catch(e => {
             throw Error(e.data)
         })
-
-        var data = fetch ? await result.json() : result
-        return {balance: result.balance, profile: result.profile, pool: result.pool, token: result.token}
     },
 }
